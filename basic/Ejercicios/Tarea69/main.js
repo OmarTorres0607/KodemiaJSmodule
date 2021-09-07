1c710cdfeb78208ba69c42c5084ac7caffac63d0
OmarTorres0607 <82553141+OmarTorres0607@users.noreply.github.com>
2021-09-07 18:44:26 -0500

    Tarea

---
+++ basic/Ejercicios/Tarea69/main.js
@@ -0,0 +1,101 @@
+/*
+Ejercicio 1:
+Deducir:
+input: anaconda
+output: true
+input: belzeebub
+output: true
+input: koder
+output: false
+input: cerveza
+output: false
+*/
+/*
+    Ejercicio 2:
+    Pedir al usuario usuario un numero entre 1 y 10
+    Imprimir la tabla de multiplicar de ese número
+    p.ej.
+    3 x 1 = 3
+    3 x 2 = 6
+*/
+// let numero = parseInt(prompt("Ingrese un numero del 1 al 10")) 
+// for (let i = 0; i <= numero; i++){
+//     let result = numero * i
+//     console.log(`${numero} X ${i} = ${result}`)
+// }
+
+/*
+Ejercicio 3:
+Pedir al usuario un numero entre 10 y 100
+Imprimir todos los numeros pares que existen entre 1 y ese numero
+*/
+// let num = ""
+// do 
+// {num = parseInt(prompt("Ingrese un numero entre 10 y 100"))
+// } while(num < 10 && num > 100 )
+// // do 
+// // {num = parseInt(prompt("Ingrese un numero entre 10 y 100"))
+// // } while(num > 100)
+
+// for (let i = 0; i <= num; i++){
+//     console.log(i)
+// }
+
+/*
+Ejercicio 4:
+Imprimir en consola la suma total de todos los digitos de una cantidad p.ej. "1234" -> 10
+*/
+let str = "1234"
+
+let long = str.length
+
+let result =""
+
+for (let i = 0; i <= long; i++){
+    result = str[i] + str[i++]         
+    }
+    console.log(result)
+
+/*
+/*
+Ejercicio 5:
+Imprimir en consola el siguiente patrón
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+*/
+
+// let str = "**********"
+
+// let long = str.length
+
+// let result =""
+
+// for (let i = 0; i <= long; i++){
+//     result += str.charAt(i)
+//         console.log(result)
+//     }
+
+/*
+    Ejercicio 6;
+    Imprimir en consola la suma de los multiplos de 3 y 5 contenidos entre el 1 y 100 -> 233168
+*/
+/*
+Ejercicio 7:
+Deducir: 
+input: Hola como estas
+output: holaComoEstas
+input: hello Koders
+output: helloKoders
+*/
+/*
+Ejercicio 8:
+Desarrollar un programa que solicite 10 números e imprima la suma de lo últimos 5 valores ingresados.
+*/
--- basic/Ejercicios/js/main.js
+++ basic/Ejercicios/js/main.js
@@ -17,11 +17,32 @@ output: "A VeRy lArGe sTrInG"
 input: "abcdefg"
 output: "AbCdEfG"
 */
-let str = "a very large string"
 
-let change = Map
+let str = "kodemia"
 
-console.log(change)
+let long = str.length
+
+let result =""
+
+for (let index = 0; index < long; index++) {
+    if (index % 2 === 0){
+         result += str.charAt(index).toLocaleUpperCase()
+    } else {
+        result += str.charAt(index).toLocaleLowerCase()
+    }
+}
+console.log(result)
+
+// while (index < long) {
+//     if (index % 2 === 0){
+//        result = result + str.charAt(index).toLocaleUpperCase()
+//     } else {
+//        result = result + str.charAt(index).toLocaleLowerCase()
+//     }
+//     index ++
+// }
+
+
 
 
 

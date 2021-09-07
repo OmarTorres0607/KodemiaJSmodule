61aa83d220db06b42dca4e6ce5c862c4f414dd57
OmarTorres0607 <82553141+OmarTorres0607@users.noreply.github.com>
2021-09-06 19:21:16 -0500

    tarea

---
+++ basic/Ejercicios/js/main.js
@@ -0,0 +1,34 @@
+/*Ejercicios */
+/*
+Deducir:
+input: "kodemia"
+output: "aimedok"
+*/
+// let palabra = "kodemia"
+
+// let palabrasep = [...palabra]
+// let palabrarev = palabrasep.reverse()
+// let resultado = palabrarev.join('')
+// console.log(resultado)
+
+/*
+input: "a very large string"
+output: "A VeRy lArGe sTrInG" 
+input: "abcdefg"
+output: "AbCdEfG"
+*/
+let str = "a very large string"
+
+let change = Map
+
+console.log(change)
+
+
+
+/*
+Crear una funcion que me indique si un string comienza con
+ -una vocal
+ -consonante
+ -numero
+ -caracter especial
+*/

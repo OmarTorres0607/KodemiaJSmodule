21a32688c575771409a6d69527437bb46fd82514
OmarTorres0607 <82553141+OmarTorres0607@users.noreply.github.com>
2021-09-20 20:44:29 -0500

    20-09

---
+++ basic/Bandas/main.js
@@ -0,0 +1,123 @@
+/*
+Ejercicio:
+*/
+const songsData = [
+    {
+        name: "¿Dónde jugarán los niños?",
+        band: "Mana",
+        releaseYear: "1992",
+        statistics: {
+            likes: 20000,
+            reproductions: 8000
+        },
+    },
+    {
+        name: "La muralla verde",
+        band: "Enanitos Verdes",
+        releaseYear: "1986",
+        statistics: {
+            likes: 21000,
+            reproductions: 19000
+        },
+    },
+    {
+        name: "Te Ví En Un Tren",
+        band: "Enanitos Verdes",
+        releaseYear: "1987",
+        statistics: {
+            likes: 20000,
+            reproductions: 23490
+        },
+    },
+    {
+        name: "Mariposa Traicionera",
+        band: "Mana",
+        releaseYear: "2002",
+        statistics: {
+            likes: 12000,
+            reproductions: 25690
+        },
+    },
+    {
+        name: "Rayando el Sol",
+        band: "Mana",
+        releaseYear: "1990",
+        statistics: {
+            likes: 12000,
+            reproductions: 18000
+        },
+    },
+    {
+        name: "La celula que explota",
+        band: "Jaguares",
+        releaseYear: "1999",
+        statistics: {
+            likes: 12000,
+            reproductions: 23421
+        },
+    },
+    {
+        name: "No dejes que...",
+        band: "Jaguares",
+        releaseYear: "1999",
+        statistics: {
+            likes: 12345,
+            reproductions: 10000
+        },
+    }
+]
+
+// - Agrupar el nombres de la bandas, que no esten repetidas
+// let Bandname = songsData.map((banda,index) => {
+//     return banda.band
+// })
+// let uniqueBand = [...new Set (Bandname)]
+// console.log(uniqueBand)
+// // - Agrupar las canciones por banda
+// const songsbyBand =(arraySong, index) => {
+    
+//     let songs = arraySong.filter((song, index)=> {song.band === uniqueBand})
+//     return songs
+// }
+
+// console.log(songsbyBand(songsData, "Jaguares"))
+// // - La cancion con más reproducciones
+// - La cancion con más likes
+
+
+// Obtener una lista de strings con el siguiente formato:
+// "La cancion {name} es de {banda}"
+
+const songwName = (arraySong) =>{
+
+    let songName = arraySong.map((banda,index) => {
+        return `La cancion ${banda.name} es de ${banda.band}`
+    })
+
+    return songName
+}
+
+console.log(songwName(songsData))
+
+// crear una funcion que me permita Obtener las canciones que contengan un criterio de busqueda
+// criterio: cel
+// [
+//     {
+//         name: "La celula que explota",
+//         band: "Jaguares",
+//         releaseYear: "1999",
+//         statistics: {
+//             likes: 12000,
+//             reproductions: 23421
+//         },
+//     }
+// ]
+
+const searcher = (arraySong, wordToSearch) =>{
+
+let matcher = arraySong.filter((song, index) => song.name.toLocaleLowerCase().includes(wordToSearch.toLocaleLowerCase()))
+
+return matcher
+}
+
+console.log(searcher(songsData,"cel"))
--- basic/S10Map/main.js
+++ basic/S10Map/main.js
@@ -6,3 +6,12 @@ let doubleNumbers = numbersArray.map((number, index) =>{
 
 console.log(numbersArray)
 console.log(doubleNumbers)
+
+let numbersArray = [2,6,7,10,5];
+
+let pares = numbersArray.filter((number,index) =>{
+    if(number % 2 === 0){
+        return number
+    }
+
+})

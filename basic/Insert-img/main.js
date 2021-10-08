612595d43787e40b2b530ba985650071ebb0c2ce
OmarTorres0607 <82553141+OmarTorres0607@users.noreply.github.com>
2021-10-08 15:57:55 -0500

    Allforone

---
+++ basic/Insert-img/main.js
@@ -0,0 +1,20 @@
+const imagePreview = document.getElementById('img-prev')
+
+const imageUploader = document.getElementById('img-uploader')
+
+const CLOUDINARY = 'https://api.cloudinary.com/v1_1/dcakvkbpz/image/upload'
+
+const Cloudinary_preset = 'vn5lewjj'
+
+imageUploader.addEventListener('change', async (event) => {
+   const file = event.target.files[0]
+   const formData = new FormData() 
+   formData.append('file', file)
+   formData.append('upload_preset', Cloudinary_preset)
+
+    const res = await axios.post(CLOUDINARY, formData, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+    })
+    console.log(res)
+    imagePreview.src = res.data.secure_url
+})
---
+++ basic/S26-JqueryAjax/js/main.js
@@ -0,0 +1,14 @@
+// $.ajax({
+//     method: "GET" 
+//     url: "endpoint" // a donde hacemos la peticion
+//     data: JSON.stringify({}) // la data que se envia en el body de la peticion - POST, PUT, PATCH//
+//     success: (response) => {
+//         //Funcion para cuando la peticion es exitosa
+//     },
+//     error: (error) => {
+//         //Funcion para cuando la peticion es incorrecta
+//     }
+
+//     async: true
+
+// })
